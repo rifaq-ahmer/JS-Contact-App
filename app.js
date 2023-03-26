482fe8b33603728d8a430cd8c10282791bd762f1
rifaq-ahmer <rifaq.ahmer@gmail.com>
2023-03-26 09:43:01 +0530

    implemented  modal functionality with lorem text

--- app.js
+++ app.js
@@ -1,1 +1,28 @@
-console.log("app.js script linked");
+const openModalButton = document.querySelector("[data-modal-target]");
+const closeModalButton = document.querySelector("[data-close-button]");
+const overlay = document.getElementById("overlay");
+
+openModalButton.addEventListener("click", () => {
+	const modal = document.querySelector(openModalButton.dataset.modalTarget);
+	openModalFn(modal);
+});
+closeModalButton.addEventListener("click", () => {
+	const modal = closeModalButton.closest(".modal");
+	closeModalFn(modal);
+});
+overlay.addEventListener("click", () => {
+	const modals = document.querySelectorAll(".modal.active");
+	modals.forEach((modal) => {
+		closeModalFn(modal);
+	});
+});
+const openModalFn = (modal) => {
+	if (modal == null) return;
+	modal.classList.add("active");
+	overlay.classList.add("active");
+};
+const closeModalFn = (modal) => {
+	if (modal == null) return;
+	modal.classList.remove("active");
+	overlay.classList.remove("active");
+};
